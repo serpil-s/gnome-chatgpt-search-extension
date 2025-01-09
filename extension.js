1802c6f84c47293c2d75a9ed65466a594f7e37f8
serpil-s <serpil.saldik@tubitak.gov.tr>
2025-01-09 16:08:53 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,31 +1,31 @@
 const { Clutter, St } = imports.gi;
 const Main = imports.ui.main;
 
-let button; // Uzantı UI bileşeni için bir değişken
+let button; // Uzantının eklediği buton
 
 function enable() {
-    // Button UI nesnesi oluştur
+    // Yeni bir buton oluştur
     button = new St.Button({
         style_class: 'system-menu-action',
         reactive: true,
         can_focus: true,
         track_hover: true,
-        label: 'AI Question Search',
+        label: 'Ask AI',
     });
 
-    // GNOME Shell UI'ya ekle
+    // GNOME Shell UI'ya butonu ekle
     Main.panel._leftBox.insert_child_at_index(button, 0);
 
-    // Tıklama olayını bağla
+    // Butona tıklama olayını bağla
     button.connect('clicked', () => {
         imports.misc.util.spawn(['python3', '/usr/share/gnome-shell/extensions/ai-question-search@serpil-s/ai_question_search.py']);
     });
 }
 
 function disable() {
-    // Button nesnesini kaldır ve temizle
+    // Oluşturulan butonu temizle
     if (button) {
-        button.destroy(); // Nesneyi yok et
-        button = null;    // Referansı temizle
+        button.destroy(); // Butonu yok et
+        button = null;    // Referansı sıfırla
     }
 }
